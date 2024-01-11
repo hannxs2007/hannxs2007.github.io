504cc35b09e9bab3a15dc05799d545a38d54d3cc
hannxs2007 <hannxs42@gmail.com>
2024-01-11 11:31:22 +0100

    fixxd button

--- Praktikum.js
+++ Praktikum.js
@@ -1,8 +1,4 @@
-function ausklappen(){
-    const TextWoche1 = document.getElementById("text_woche_1")
-    TextWoche1.innerHTML= "text_woche_1"
-
-    const button = document.getElementById("Button_Text");
-    button.addEventListener("click", ausklappen)
-
-}
+document.getElementById('Button_Text').addEventListener('click', function() {
+    document.getElementById('text_woche_1').style.display =
+      document.getElementById('text_woche_1').style.display === 'none' ? 'block' : 'none';
+ });
