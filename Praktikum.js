7cab1d59ff2173e83e93230c1dff63f19b2967fc
hannxs2007 <hannxs42@gmail.com>
2024-01-11 14:11:53 +0100

    fixed

--- Praktikum.js
+++ Praktikum.js
@@ -2,3 +2,11 @@ document.getElementById('Button_Text').addEventListener('click', function() {
     document.getElementById('text_woche_1').style.display =
       document.getElementById('text_woche_1').style.display === 'none' ? 'block' : 'none';
  });
+
+ document.documentElement.className = localStorage.getItem('theme') || 'dark-mode';
+
+function toggleTheme() {
+ document.documentElement.classList.toggle('dark-mode');
+ document.documentElement.classList.toggle('light-mode');
+ localStorage.setItem('theme', document.documentElement.className);
+}
